5eb079805bbe41119cf0ea0dea086511aba8df27
kdncode <kdncode@gmail.com>
2018-04-21 01:38:17 -0500

    added remove function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Check off specific task
 $('li').click(function() {
     $(this).toggleClass('completed')
+})
+
+// Click on X to remove task
+$('span').click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    })
+    event.stopPropagation()
 })
