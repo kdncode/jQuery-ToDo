eca1f4d748f633b1ae4b954b3cfdce0f7565a257
kdncode <kdncode@gmail.com>
2018-04-21 01:24:39 -0500

    replaced new method using toggleClass with just 1 line

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
 $('li').click(function() {
-
-    // If li is gray
-    if ( $(this).css('color') === 'rgb(128, 128, 128)') {
-
-        // Turn it black
-        $(this).css({
-            color: 'black',
-            textDecoration: 'none'
-           
-        });
-
-    } else {
-
-        // Else Turn it gray
-        $(this).css({
-            color: 'gray',
-            textDecoration: 'line-through'
-        });
-    }
-
+    $(this).toggleClass('completed')
 })
